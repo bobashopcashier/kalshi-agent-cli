cc044db190fdd7c2eb6c1bfbf796ffafecbd86d9
bobashopcashier <kennyx102@gmail.com>
2026-08-01 22:32:54 -0700

    fix: report module version for go install

--- cmd/kalshi/main.go
+++ cmd/kalshi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"runtime/debug"
 
 	"github.com/bobashopcashier/kalshi-cli/internal/cli"
 )
@@ -14,10 +15,28 @@ func main() {
 		Stdin:   os.Stdin,
 		Stdout:  os.Stdout,
 		Stderr:  os.Stderr,
-		Version: version,
+		Version: buildVersion(),
 		IsTTY:   isTerminal(os.Stdout),
 	})
 	os.Exit(app.Run(context.Background(), os.Args[1:]))
+}
+
+func buildVersion() string {
+	info, ok := debug.ReadBuildInfo()
+	return resolveVersion(version, info, ok)
+}
+
+func resolveVersion(injected string, info *debug.BuildInfo, ok bool) string {
+	if injected != "" && injected != "dev" {
+		return injected
+	}
+	if ok && info != nil && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+	if injected != "" {
+		return injected
+	}
+	return "dev"
 }
 
 func isTerminal(f *os.File) bool {
